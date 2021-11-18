20b8fa323458a930ca95ea2d0108d677d268c8f5
einwickler <29920253+einwickler@users.noreply.github.com>
2021-11-18 23:42:13 +0100

    Removed unused lines and added comments

--- .reattach_main_tmux.sh
+++ .reattach_main_tmux.sh
@@ -3,17 +3,20 @@
 main_session_name=main
 session_info=$(tmux list-sessions)
 
+# Checking if main session exists
 if tmux has-session -t $main_session_name 2> /dev/null;
 then
     unat_sessions=$(echo "$session_info" | grep -v " (attached)$")
     main_session_unat=$(echo "$unat_sessions" | grep "^$main_session_name: ")
 
+    # Checking if main session is unattached
     if [[ $main_session_unat ]]; then
         echo "Attaching to main: '${main_session_unat}'"
         tmux attach-session -t $main_session_name
     else
-        # Check if unattached session available
         ses_to_attach=$(echo "$unat_sessions" | cut -d: -f1 | head -n1)
+
+        # Check if any other unattached session is available
         if [[ $ses_to_attach ]]; then
             echo "Attaching to session: '${ses_to_attach}'"
             tmux attach-session -t $ses_to_attach    
@@ -26,5 +29,4 @@ else
     "Creating new main session: ${main_session_name}"
     tmux new-session -s $main_session_name
 fi
-# tmux list-sessions -F "#S #{session_attached}" | grep main | sed -En "/1/p"
-tmux attach-session main
+
