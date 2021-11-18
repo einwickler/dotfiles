56b30caf2b4147f29eeef3f15d8e1842051b80e9
einwickler <29920253+einwickler@users.noreply.github.com>
2021-11-18 23:36:29 +0100

    Reattach to unattached sessions when main already attached

--- .reattach_main_tmux.sh
+++ .reattach_main_tmux.sh
@@ -1,10 +1,30 @@
-# Return if not in interactive shell or already in tmux
-[[ ($- != *i* ||  -n "$TMUX") ]] && return 
+#!/bin/bash
 
+main_session_name=main
+session_info=$(tmux list-sessions)
 
-if tmux has-session -t main 2> /dev/null;
+if tmux has-session -t $main_session_name 2> /dev/null;
 then
-    tmux attach-session -t main
+    unat_sessions=$(echo "$session_info" | grep -v " (attached)$")
+    main_session_unat=$(echo "$unat_sessions" | grep "^$main_session_name: ")
+
+    if [[ $main_session_unat ]]; then
+        echo "Attaching to main: '${main_session_unat}'"
+        tmux attach-session -t $main_session_name
+    else
+        # Check if unattached session available
+        ses_to_attach=$(echo "$unat_sessions" | cut -d: -f1 | head -n1)
+        if [[ $ses_to_attach ]]; then
+            echo "Attaching to session: '${ses_to_attach}'"
+            tmux attach-session -t $ses_to_attach    
+        else
+            echo "Creating new session '${ses_to_attach}'"
+            tmux new-session
+        fi
+    fi
 else
-    tmux new-session -s main
+    "Creating new main session: ${main_session_name}"
+    tmux new-session -s $main_session_name
 fi
+# tmux list-sessions -F "#S #{session_attached}" | grep main | sed -En "/1/p"
+tmux attach-session main
