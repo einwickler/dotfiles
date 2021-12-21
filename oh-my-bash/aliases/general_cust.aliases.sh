a2d3b814b01ff07c613b80d2e52282d3b491499c
einwickler <29920253+einwickler@users.noreply.github.com>
2021-12-21 17:22:04 +0100

    Added custom aliases file

---
+++ oh-my-bash/aliases/general_cust.aliases.sh
@@ -0,0 +1,3 @@
+# Add your own custom alias in the custom/aliases directory. Aliases placed
+# here will override ones with the same name in the main alias directory.
+alias clearhist="cat /dev/null >| ~/.bash_history && history -c && exit"
